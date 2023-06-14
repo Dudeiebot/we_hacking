ff3fcb2c206b7cf21cdd70149ce1140c7535f8c2
Dudeiebot <ojoexcel5@gmail.com>
2023-06-14 22:25:38 +0100

    channels

--- channels/main.go
+++ channels/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func DoWork() int {
+	time.Sleep(time.Second)
+	return rand.Intn(50)
+}
 
 func main() {
-	fmt.Printf("Welcome to the go")
+	dataChan := make(chan int)
+
+	go func() { // a  buffered go routine or maybe you add the require output to dataChan i.e dataChan := make(chan int 2)
+		wg := &sync.WaitGroup{}
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				result := DoWork()
+				dataChan <- result
+			}()
+		}
+		wg.Wait()
+		close(dataChan)
+	}()
+
+	for n := range dataChan {
+		fmt.Printf("n : %d\n", n)
+	}
+
 }
