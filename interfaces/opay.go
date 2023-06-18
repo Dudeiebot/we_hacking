90a67f41da2eeae3c67e2345ea16416a3de5a94e
Dudeiebot <ojoexcel5@gmail.com>
2023-06-18 17:11:41 +0100

    added Opay

---
+++ interfaces/opay.go
@@ -0,0 +1,31 @@
+package main
+
+import "errors"
+
+type Opay struct {
+	balance float64
+	fee     float64
+}
+
+func NewOpay() *Opay {
+	return &Opay{
+		balance: 0,
+		fee:     10,
+	}
+}
+func (o *Opay) GetBalance() float64 {
+	return o.balance
+}
+
+func (o *Opay) Deposit(amount float64) {
+	o.balance += amount
+}
+
+func (o *Opay) Withdraw(amount float64) error {
+	newBalance := o.balance - amount - o.fee
+	if newBalance < 0 {
+		return errors.New("insufficient balance")
+	}
+	o.balance = newBalance
+	return nil
+}
