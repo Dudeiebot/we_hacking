c7eee952c11e03a011636633ea6fe7b7123a4264
Dudeiebot <ojoexcel5@gmail.com>
2023-06-22 14:40:35 +0100

    Generics

--- Generics/main.go
+++ Generics/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+func Add[T constraints.Ordered](a T, b T) T {
+	return a + b
+}
+
+func main() {
+	result := Add(1.1, 2.2)
+	fmt.Printf("result: %+v\n", result)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/gorilla/mux v1.8.0
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
 )
 
 require (
