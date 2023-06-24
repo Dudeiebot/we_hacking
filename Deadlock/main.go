5d38d38375b825422343186930321dcd14e11e52
Dudeiebot <ojoexcel5@gmail.com>
2023-06-24 18:27:13 +0100

    deadlocks

--- Deadlock/main.go
+++ Deadlock/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"appengine/memcache"
+)
+
+func main() {
+
+	type dude struct {
+		heisadude string
+		notadude  string
+		err       error
+	}
+	dataChan := make(chan dude) //created my data type here and added it to my channel
+
+	dataChan <- dude{}
+	go func() {
+
+	}()
+
+	go func() {
+
+	}()
+
+	go func() {
+
+	}()
+
+	if Err != memcache.ErrCacheMiss {
+		c.Errorf("memcache get %q: %v", ID, err)
+	}
+}
