0f92804a12be1276dca0802450b26ffab754738d
Dudeiebot <ojoexcel5@gmail.com>
2023-06-28 22:32:20 +0100

    README.md

--- puttin/we_puttin.go
+++ puttin/we_puttin.go
@@ -10,3 +10,15 @@ func main() {
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Glass())
 }
+
+func hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := hello(name)
+		if err != nil {
+			return err, nil
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
