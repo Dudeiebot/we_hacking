b228a348709decfca6b6f211b117d365167f08b3
Dudeiebot <ojoexcel5@gmail.com>
2023-10-17 21:29:55 +0100

    just added more comment to the main

--- channels/main.go
+++ channels/main.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+// data := <- a read from channel a
+// a <- data  write to channel a
+
 func DoWork() int {
 	time.Sleep(time.Second)
 	return rand.Intn(50)
@@ -14,7 +17,7 @@ func DoWork() int {
 
 // Like maps and slices, channels must be created before use, that is where the make chan comes in.
 func main() {
-	dataChan := make(chan int) //Channel can be buffered "make (chan int, 3)"
+	dataChan := make(chan int) // Channel can be buffered "make (chan int, 3)"
 
 	go func() { // a  new go routine cause by default a channel doesnot have space to store data or maybe you add the require space storage to dataChan i.e dataChan := make(chan int 2)
 		wg := &sync.WaitGroup{}
@@ -22,7 +25,7 @@ func main() {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				//defer wg.Done()  so these is just added when my wg.add(2) and we need to tell it we are done twice and these are just for testing sake and getting thing safe and done
+				// defer wg.Done()  so these is just added when my wg.add(2) and we need to tell it we are done twice and these are just for testing sake and getting thing safe and done
 				result := DoWork()
 				dataChan <- result
 			}()
@@ -34,5 +37,4 @@ func main() {
 	for n := range dataChan {
 		fmt.Printf("n : %d\n", n)
 	}
-
 }
