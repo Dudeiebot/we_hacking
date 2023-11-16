7012fd2991bfe37d72db347aac6e67704d241d51
Dudeiebot <ojoexcel5@gmail.com>
2023-11-16 22:17:13 +0100

    adding table driven approach to the test

--- interfaces/access_test.go
+++ interfaces/access_test.go
@@ -38,3 +38,48 @@ func TestGetName(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+// Table driven approach
+
+//func checkBalance(t *testing.T, access Access, want float64) {
+// 	t.Helper()
+// 	got := access.balance
+//
+// 	if got != want {
+// 		t.Errorf("got %v want %v", got, want)
+// 	}
+// }
+//
+// func TestAccess(t *testing.T) {
+// 	tests := []struct {
+// 		name      string
+// 		initial   float64
+// 		operation func(*Access)
+// 		want      float64
+// 	}{
+// 		{"Get balance", 100.00, nil, 100.00},
+// 		{"Deposit", 120.50, func(a *Access) { a.Deposit(50) }, 170.50},
+// 		{"Withdraw", 50.00, func(a *Access) { a.Withdraw(20) }, 30.00},
+// 	}
+//
+// 	for _, test := range tests {
+// 		t.Run(test.name, func(t *testing.T) {
+// 			access := Access{balance: test.initial}
+// 			if test.operation != nil {
+// 				test.operation(&access)
+// 			}
+// 			checkBalance(t, access, test.want)
+// 		})
+// 	}
+// }
+//
+// func TestGetName(t *testing.T) {
+// 	t.Run("Get Name", func(t *testing.T) {
+// 		got := NewAccess().GetName()
+// 		want := "Access"
+//
+// 		if got != want {
+// 			t.Errorf("got %v want %v", got, want)
+// 		}
+// 	})
+// }
